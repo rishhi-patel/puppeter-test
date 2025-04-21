00ffb620692d4437125042835bd9e15ea797ce15
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2025-04-21 15:38:36 -0400

    -- updated puppeter build

--- src/app/api/pptr/route.js
+++ src/app/api/pptr/route.js
@@ -1,50 +1,50 @@
-import chromium from "@sparticuz/chromium-min";
-import puppeteerCore from "puppeteer-core";
-import puppeteer from "puppeteer";
+import chromium from "@sparticuz/chromium-min"
+import puppeteerCore from "puppeteer-core"
+import puppeteer from "puppeteer"
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"
 
 const remoteExecutablePath =
-  "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar";
+  "https://github.com/Sparticuz/chromium/releases/download/v133.0.0/chromium-v133.0.0-pack.tar"
 
-let browser;
+let browser
 
 async function getBrowser() {
-  if (browser) return browser;
+  if (browser) return browser
 
   if (process.env.NEXT_PUBLIC_VERCEL_ENVIRONMENT === "production") {
     browser = await puppeteerCore.launch({
       args: chromium.args,
       executablePath: await chromium.executablePath(remoteExecutablePath),
       headless: true,
-    });
+    })
   } else {
     browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
       headless: true,
-    });
+    })
   }
-  return browser;
+  return browser
 }
 
 async function checkPageStatus(url) {
-  let statusCode;
+  let statusCode
   try {
-    const browser = await getBrowser();
-    const page = await browser.newPage();
-    const response = await page.goto(url, { waitUntil: "domcontentloaded" });
-    statusCode = response && response.status() === 200 ? 200 : 404;
-    await page.close();
+    const browser = await getBrowser()
+    const page = await browser.newPage()
+    const response = await page.goto(url, { waitUntil: "domcontentloaded" })
+    statusCode = response && response.status() === 200 ? 200 : 404
+    await page.close()
   } catch (error) {
-    console.error("Error accessing page:", error);
-    statusCode = 404;
+    console.error("Error accessing page:", error)
+    statusCode = 404
   }
-  return statusCode === 200;
+  return statusCode === 200
 }
 
 export async function GET(request) {
-  const { searchParams } = new URL(request.url);
-  const url = searchParams.get("url");
+  const { searchParams } = new URL(request.url)
+  const url = searchParams.get("url")
   if (!url) {
     return new Response(
       JSON.stringify({ error: "URL parameter is required" }),
@@ -52,9 +52,9 @@ export async function GET(request) {
         status: 400,
         headers: { "Content-Type": "application/json" },
       }
-    );
+    )
   }
-  const status = await checkPageStatus(url);
+  const status = await checkPageStatus(url)
   return new Response(
     JSON.stringify({
       statusCode: status ? 200 : 404,
@@ -64,5 +64,5 @@ export async function GET(request) {
       status: status ? 200 : 404,
       headers: { "Content-Type": "application/json" },
     }
-  );
+  )
 }
